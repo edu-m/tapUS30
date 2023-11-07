79fb1fd71ea2a52e67d5cfe5c914c39174d0dd7a
edu-m <mledrd02e15c351o@studium.unict.it>
2023-11-07 17:32:12 +0100

    organized into folders

--- Fluentd/fluentrun.sh
+++
@@ -1,1 +0,0 @@
-docker run -ti --rm -v "$PWD":/fluentd/etc fluentkafka -c /fluentd/etc/fluent.conf
--- kafka/kafka_init/init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-kafka-topics.sh --bootstrap-server localhost:9092 --create --if-not-exists --topic tech --replication-factor 1 --partitions 1
-kafka-topics.sh --bootstrap-server localhost:9092 --create --if-not-exists --topic cgoods --replication-factor 1 --partitions 1
-kafka-topics.sh --bootstrap-server localhost:9092 --create --if-not-exists --topic energy --replication-factor 1 --partitions 1
-kafka-topics.sh --bootstrap-server localhost:9092 --create --if-not-exists --topic financial --replication-factor 1 --partitions 1
-kafka-topics.sh --bootstrap-server localhost:9092 --create --if-not-exists --topic health --replication-factor 1 --partitions 1
-kafka-topics.sh --bootstrap-server localhost:9092 --create --if-not-exists --topic industrial --replication-factor 1 --partitions 1
