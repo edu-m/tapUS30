4434cb25c46347343d1ff65a9f71c19559c8053e
edu-m <mledrd02e15c351o@studium.unict.it>
2023-11-15 17:34:43 +0100

    added data processing & doc

--- batch/Spark/app/process.py
+++ batch/Spark/app/process.py
@@ -5,12 +5,16 @@ from pyspark import SparkFiles
 import pandas as pd
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
+from pyspark.ml.functions import vector_to_array
 from pyspark.ml.linalg import Vectors
+from pyspark.sql.types import TimestampType
 from pyspark.ml.feature import VectorAssembler
 from elasticsearch import Elasticsearch
 from pyspark.ml.regression import LinearRegression
 from pyspark.ml.feature import StringIndexer
-import time
+from datetime import datetime
+from pyspark.sql import Window
+from pyspark.sql import functions as F
 
 sc = SparkContext(appName="tapUS30")
 spark = SparkSession(sc)
@@ -19,21 +23,45 @@ sc.setLogLevel("WARN")
 names=["cgoods","financial","energy","health","industrial","tech"]
 indexes=[Elasticsearch("http://es_cgoods:9200"),Elasticsearch("http://es_financial:9200"),Elasticsearch("http://es_energy:9200"),Elasticsearch("http://es_health:9200"),Elasticsearch("http://es_industrial:9200"),Elasticsearch("http://es_tech:9200")]
 models=[]
+day_in_ms = 86400000
+
+def recursive_predict(iters, trained_model, df, predictions=None):
+    if iters == 0:
+        return predictions
+    max_t = int(output.select(F.max("timestamp")).first()[0])
+    window_spec = Window.partitionBy("tickerSymbol").orderBy(col("timestamp").desc())
+    print(df)
+    df_with_row_number = df.withColumn("row_num", row_number().over(window_spec))
+    # Filter the rows where the row number is less than or equal to 7
+    result_df = df_with_row_number.filter(col("row_num") <= 7)  
+    result_df = result_df.drop("row_num")
+    result_df = result_df.groupBy("tickerSymbol").agg(F.avg("open").alias("avg(open)"),\
+        F.avg("high").alias("avg(high)"),F.avg("low").alias("avg(low)"),F.avg("timestamp"))
+    assembler = VectorAssembler(inputCols=['avg(open)','avg(high)','avg(low)'],outputCol='features')
+    result_df = assembler.transform(result_df).select("features","tickerSymbol")
+    predictions = trained_model.transform(result_df)
+    return recursive_predict(iters-1,trained_model,result_df,predictions)
 
 for name in names:
     df = spark.read.json("/data/"+name)
     assembler = VectorAssembler(inputCols=['open','high','low'],outputCol='features')
-    output = assembler.transform(df).select('features','close','tickerSymbol')
-    splits = output.randomSplit([0.7,0.3])
-    train_df = splits[0]
-    test_df = splits[1]
+    output = assembler.transform(df).select('features','close','open','high','low','tickerSymbol','timestamp')
+    
     lr = LinearRegression (featuresCol='features',labelCol='close',maxIter=10,regParam=0.3,elasticNetParam=0.8)
-    trained_model = lr.fit(train_df)
-    predictions = trained_model.transform(output)
-    predictions = predictions.select("close","prediction")
-    lr.save("/models/"+name)
-    models.append(predictions.toPandas().to_dict(orient="records"))
+    trained_model = lr.fit(output)
+    predictions = recursive_predict(7,trained_model,output)
+    all_columns = output.columns + predictions.columns
+    merged_df = output.join(predictions, on=["tickerSymbol","features"],how="full")
+    merged_df = merged_df.fillna({"timestamp": max_t+day_in_ms})
+    merged_df = merged_df.fillna({"close": 0})
+    merged_df = merged_df.fillna({"prediction": 0})
+    merged_df.filter(col("prediction").isNotNull()).show()
+    lr.write().overwrite().save("/models/"+name)
+    formatted_data = merged_df.groupBy("timestamp").avg("close","prediction").sort("timestamp").withColumn("timestamp",merged_df.timestamp / 1000).withColumn("timestamp", col("timestamp").cast(TimestampType())).\
+    withColumn("timestamp",date_format(col("timestamp"),"yyyy-MM-dd")).withColumn("avg(close)",round(col("avg(close)"),2))
+    models.append(formatted_data.toPandas().to_dict(orient="records"))
 
 for i in range(len(indexes)):
+    print(i)
     for j in range(len(models[i])):
         indexes[i].index(index=names[i], id=j,document=json.dumps(models[i][j]))
